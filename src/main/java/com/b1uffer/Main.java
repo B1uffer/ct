9cdbe2d8a465640c6db65057169d94e4490a3a13
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-27 17:16:11 +0900

    main 수정

--- src/main/java/com/b1uffer/Main.java
+++ src/main/java/com/b1uffer/Main.java
@@ -1,9 +1,13 @@
 package com.b1uffer;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] array = {1,1,2,2};
-        Mode mode = new Mode();
-        mode.solution(array);
+        int n = 54;
+        Hate hate = new Hate();
+        System.out.println(Arrays.toString(hate.solution(n)));
+//        System.out.println(15/2);
     }
 }
