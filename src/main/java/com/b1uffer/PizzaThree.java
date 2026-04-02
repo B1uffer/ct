92ab9094648063d88abca4d7e861e903e27e6bf6
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-02 23:12:50 +0900

    주석 추가

--- src/main/java/com/b1uffer/PizzaThree.java
+++ src/main/java/com/b1uffer/PizzaThree.java
@@ -1,6 +1,13 @@
 package com.b1uffer;
 
 public class PizzaThree {
+    /**
+     * 머쓱이네 피자가게
+     * 피자를 두조각에서 열조각까지 원하는 조각 수로 잘라준다
+     * 피자 조각 수 slice, 피자를 먹는 사람의 수 n이 매개변수
+     * n명의 사람이 최소 한 조각 이상 피자를 먹으려면 최소 몇판의 피자를 시켜야 하는가?
+     */
+
     public int solution(int slice, int n) { // n이 먹는 사람의 수, slice는 피자 조각 수
         int answer = 0;
         int pizza = 1;
