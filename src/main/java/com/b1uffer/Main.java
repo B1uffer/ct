f1c47f179c4e31d6ab7c9f90b48dfbe0c6cbc518
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-07 15:33:40 +0900

    Main 수정

--- src/main/java/com/b1uffer/Main.java
+++ src/main/java/com/b1uffer/Main.java
@@ -1,11 +1,14 @@
 package com.b1uffer;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         int a = 4;
         int n = 12;
-        PizzaThree pizza = new PizzaThree();
-        System.out.println(pizza.solution(a,n));
+        int money = 5000;
+        IceCoffee coffee = new IceCoffee();
+        System.out.println(Arrays.toString(coffee.solution(money)));
     }
 }
