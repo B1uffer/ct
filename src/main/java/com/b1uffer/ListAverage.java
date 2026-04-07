cc7e7026d69f08f73bf6f87be47ed30dae6e6c39
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-07 13:14:54 +0900

    ListAverage 구현

--- src/main/java/com/b1uffer/ListAverage.java
+++ src/main/java/com/b1uffer/ListAverage.java
@@ -1,4 +1,23 @@
 package com.b1uffer;
 
+import java.util.Arrays;
+
 public class ListAverage {
+    public double solution(int[] numbers) {
+        /**
+         * 정수배열 numbers가 매개변수로 주어진다
+         * numbers의 원소의 평균값을 return하도록 solution함수를 완성하세요
+         */
+        double answer = 0;
+        double sum = 0;
+
+        Arrays.stream(numbers).average().orElse(0);
+
+        for(int i = 0; i< numbers.length; i++) {
+            sum += numbers[i];
+            answer = sum / numbers.length;
+        }
+
+        return answer;
+    }
 }
