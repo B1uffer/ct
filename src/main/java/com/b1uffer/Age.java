d761155a0c95bac3be8e5f6af9c75d2aff3382ed
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-08 23:55:37 +0900

    나이 출력 Age 클래스 추가

--- src/main/java/com/b1uffer/Age.java
+++ src/main/java/com/b1uffer/Age.java
@@ -1,4 +1,15 @@
 package com.b1uffer;
 
+import java.util.*;
+
 public class Age {
+    /**
+     * 선생님이 몇년도에 태어났는지 궁금함
+     * 2022년 기준 선생님의 나이 age가 주어질 때
+     * 선생님의 출생 연도를 return하는 solution함수 완성하기
+     */
+    public int solution(int age) {
+        int answer = 0;
+        return answer;
+    }
 }
