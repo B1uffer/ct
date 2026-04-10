3ab81a1495a3e9cb723b633132b9832256c6308d
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-10 20:56:42 +0900

    나이 출력 풀이

--- src/main/java/com/b1uffer/Age.java
+++ src/main/java/com/b1uffer/Age.java
@@ -7,9 +7,14 @@ public class Age {
      * 선생님이 몇년도에 태어났는지 궁금함
      * 2022년 기준 선생님의 나이 age가 주어질 때
      * 선생님의 출생 연도를 return하는 solution함수 완성하기
+     * 0 < age <= 120
+     * 나이는 태어난 년도에 1살이며, 매년 1월 1일마다 1살씩 증가한다
      */
     public int solution(int age) {
         int answer = 0;
+        int year = 2022;
+        answer = year - age + 1;
+
         return answer;
     }
 }
--- src/main/java/com/b1uffer/Main.java
+++ src/main/java/com/b1uffer/Main.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
-        int a = 4;
+        int a = 40;
         int n = 12;
         int money = 5000;
-        IceCoffee coffee = new IceCoffee();
-        System.out.println(Arrays.toString(coffee.solution2(money)));
+        Age coffee = new Age();
+        System.out.println(coffee.solution(a));
     }
 }
