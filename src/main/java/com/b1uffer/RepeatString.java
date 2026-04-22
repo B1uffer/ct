8d8fa3088e8fabfa7692fd84d65c4cb48095ae3a
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-22 16:40:01 +0900

    문자열 반복 출력 구현

--- src/main/java/com/b1uffer/RepeatString.java
+++ src/main/java/com/b1uffer/RepeatString.java
@@ -9,6 +9,12 @@ public class RepeatString {
      */
     public String solution(String my_string, int n) {
         String answer = "";
+        for(int i = 0; i < my_string.length(); i++) {
+            char str = my_string.charAt(i);
+            for(int j = 0; j < n; j++) {
+                answer += str;
+            }
+        }
         return answer;
     }
 }
