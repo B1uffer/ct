e1e4dc106ca114000662f81ac2d161799f978f1e
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-22 16:31:08 +0900

    짝수 홀수 개수 구현

--- src/main/java/com/b1uffer/NumOfNumList.java
+++ src/main/java/com/b1uffer/NumOfNumList.java
@@ -6,8 +6,23 @@ public class NumOfNumList {
      * num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을
      * return하도록 solution함수 완성하기
      */
-    public int[] solution(int[] num_list) {
+    public int[] solution(int[] num_list) { // 리스트
         int[] answer = {};
+        answer = new int[2];
+        int a = 0;
+        int b = 0;
+
+        for(int i = 0; i < num_list.length; i++) {
+            int number = num_list[i];
+            if(number % 2 == 0) {
+                a++;
+            } else {
+                b++;
+            }
+        }
+        answer[0] = a;
+        answer[1] = b;
+
         return answer;
     }
 }
