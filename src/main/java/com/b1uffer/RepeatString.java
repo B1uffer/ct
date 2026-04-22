896dc84a63ffbaf5c5690b078d82ca1254831799
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-22 16:34:37 +0900

    주석 수정

--- src/main/java/com/b1uffer/RepeatString.java
+++ src/main/java/com/b1uffer/RepeatString.java
@@ -5,6 +5,7 @@ public class RepeatString {
      * 문자열 my_string과 정수 n이 매개변수로 주어질 때,
      * my_string에 들어 있는 각 문자를 n만큼 반복한 문자열을
      * return하도록 함수 완성하기
+     * my_string이 "hello", n = 3이면 결과는 "hhheeellllllooo"
      */
     public String solution(String my_string, int n) {
         String answer = "";
