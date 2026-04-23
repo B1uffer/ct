c7c2834b3c21fe809875ef5be3ffb0dd635c1c03
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-23 23:52:34 +0900

    특정 문자 제거하기 클래스 생성

---
+++ src/main/java/com/b1uffer/DeleteString.java
@@ -0,0 +1,12 @@
+package com.b1uffer;
+
+public class DeleteString {
+    /**
+     * 특정 문자 my_string과 문자 letter이 매개변수
+     * my_string에서 letter를 제거한 문자열을 return하기
+     */
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        return answer;
+    }
+}
