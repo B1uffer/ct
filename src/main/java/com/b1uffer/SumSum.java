90b7d7172489dfd6349b87592eca301528465b6e
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-27 18:02:41 +0900

    짝수의 합 클래스 생성

---
+++ src/main/java/com/b1uffer/SumSum.java
@@ -0,0 +1,12 @@
+package com.b1uffer;
+
+public class SumSum {
+    /**
+     * 정수 n이 주어질 때, n 이하의 짝수를 모두 더한값을
+     * return하도록 solution 함수 작성하기
+     */
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
