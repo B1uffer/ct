1ee2493e78142a396f17214b4a1f85cac1477d36
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-27 17:22:34 +0900

    양꼬치 클래스 생성

---
+++ src/main/java/com/b1uffer/Sheep.java
@@ -0,0 +1,15 @@
+package com.b1uffer;
+
+public class Sheep {
+    /**
+     * 머쓱이네 양꼬치 가게
+     * 10인분을 먹으면 음료수 하나를 서비스로 준다
+     * 양꼬치는 1인분에 12,000원, 음료수는 2,000원
+     * 정수 n과 k가 매개변수로 주어질 때
+     * 양꼬치 n인분과 음료수 k개를 먹었다면 총 얼마를 지불해야 하는가?
+     */
+    public int solition(int n, int k) {
+        int answer = 0;
+        return answer;
+    }
+}
