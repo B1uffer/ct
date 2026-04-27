1e6f82cb4d238fed5e62972bde963a41c1df87e1
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-27 18:31:22 +0900

    짝수의 합 구현

--- src/main/java/com/b1uffer/SumSum.java
+++ src/main/java/com/b1uffer/SumSum.java
@@ -7,6 +7,11 @@ public class SumSum {
      */
     public int solution(int n) {
         int answer = 0;
+        for(int i = 1; i <= n; i++) {
+            if(i % 2 == 0) { // 1
+                answer += i;
+            }
+        }
         return answer;
     }
 }
