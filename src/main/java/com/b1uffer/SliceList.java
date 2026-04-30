8601e96ab7d2e16ce8ae063cb43269fd3612fb0f
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-30 22:44:53 +0900

    배열 자르기 클래스 생성

---
+++ src/main/java/com/b1uffer/SliceList.java
@@ -0,0 +1,13 @@
+package com.b1uffer;
+
+public class SliceList {
+    /**
+     * 정수 배열 numbers와 정수 num1, num2가 매개변수
+     * numbers의 num1번째 인덱스부터
+     * num2번째 인덱스까지 자른 정수배열을 return하기
+     */
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = {};
+        return answer;
+    }
+}
