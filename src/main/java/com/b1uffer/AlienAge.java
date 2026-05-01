3080c013820f3270a765023d6bd8d265e886e6e1
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-01 09:51:39 +0900

    외계행성의 나이 클래스 생성

---
+++ src/main/java/com/b1uffer/AlienAge.java
@@ -0,0 +1,15 @@
+package com.b1uffer;
+
+public class AlienAge {
+    /**
+     * 우주여행을 하던 머쓱이, 엔진 고장으로 PROGRAMMERS-962 행성에 불시착함!
+     * 입국심사에서 나이를 말해야 하는데, 이 행성에서는 나이를 알파벳으로 말한다
+     * a는 0, b는 1, c는 2, ..., j는 9임
+     * 23살은 cd, 51살은 fb으로 표현한다
+     * 나이 age가 매개변수로 주어질 때, 이 행성에 기반한 나이를 return하기
+     */
+    public String solution(int age) {
+        String answer = "";
+        return answer;
+    }
+}
