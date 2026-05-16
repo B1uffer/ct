6d222b9fb8a2b8e3ea00e34d1cd997aaadb4f391
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-16 16:03:34 +0900

    외계행성의 나이 구현

--- src/main/java/com/b1uffer/AlienAge.java
+++ src/main/java/com/b1uffer/AlienAge.java
@@ -10,6 +10,12 @@ public class AlienAge {
      */
     public String solution(int age) {
         String answer = "";
+        String alienAge = "abcdefghij";
+        String[] ageArr = String.valueOf(age).split("");
+
+        for(int i=0; i<ageArr.length; i++) {
+            answer += alienAge.charAt(Integer.parseInt(ageArr[i]));
+        }
         return answer;
     }
 }
