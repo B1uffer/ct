0d950cf4a27e493fefa84585d1336d2595bbd0a3
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-18 17:24:52 +0900

    진료 순서 정하기 클래스 생성

---
+++ src/main/java/com/b1uffer/DoctorList.java
@@ -0,0 +1,14 @@
+package com.b1uffer;
+
+public class DoctorList {
+    /**
+     * 외과의사 머쓱이, 응급실에 온 환자의 응급도를 기준으로 진료 순서를 정한다
+     * 정수 배열 emergency 가 매개변수로 주어질 때,
+     * 응급도가 높은 순서대로 진료 순서를 정한 배열을 return 하도록 solution 함수 완성하기
+     */
+    public int[] solution(int[] emergency) {
+        int[] answer = {};
+
+        return answer;
+    }
+}
