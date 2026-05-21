8d9eb52ddb56e2776e8880f6ab4605ed0c51badf
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-21 22:58:57 +0900

    순서쌍 구하기 구현

--- src/main/java/com/b1uffer/NumberOfTwins.java
+++ src/main/java/com/b1uffer/NumberOfTwins.java
@@ -1,6 +1,8 @@
 package com.b1uffer;
 
-public class numberOfTwins {
+import java.util.*;
+
+public class NumberOfTwins {
     /**
      * 순서쌍이란, 두개의 숫자를 순서를 정하여 짝지어 나타낸 쌍으로 (a,b)로 표현한다
      * 자연수 n 이 매개변수로 주어질 때,
@@ -11,6 +13,20 @@ public class numberOfTwins {
      */
     public int solution(int n) {
         int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=1; i<=n; i++) {
+            if(n%i == 0) {
+                map.put(i, n/i);
+            }
+        }
+
+        answer = map.size();
         return answer;
+    }
+
+    public static void main(String[] args) {
+        NumberOfTwins twins = new NumberOfTwins();
+        System.out.println(twins.solution(25));
     }
 }
