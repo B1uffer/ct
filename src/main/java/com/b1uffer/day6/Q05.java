50097a5538411a80e3160a669dbdaefb8202222e
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-07-14 02:37:20 +0900

    day6 수열과 구간 쿼리 2 아직 못풀었음

--- src/main/java/com/b1uffer/day6/Q05.java
+++ src/main/java/com/b1uffer/day6/Q05.java
@@ -1,4 +1,31 @@
 package com.b1uffer.day6;
 
+import java.util.*;
+
 public class Q05 {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        int[] ar = new int[queries.length];
+        int min = -1;
+
+        for(int i=0; i<queries.length; i++) {
+            for(int j = queries[i][0]; j <= queries[i][1]; j++) {
+                int k = queries[i][2];
+                if(arr[j] > k) {
+                    if(arr[j] < min) {
+                        min = arr[j];
+                    }
+                }
+            }
+        }
+        answer = ar;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Q05 q = new Q05();
+        int[] ar = {0,1,2,4,3};
+        int[][] arar = {{0,4,2},{0,3,2},{0,2,2}};
+        System.out.println(Arrays.toString(q.solution(ar,arar)));
+    }
 }
