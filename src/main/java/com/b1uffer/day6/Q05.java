54921bf435b14cb973a7f986fef3a3c84665c132
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-07-15 23:13:06 +0900

    day6 수열과 구간 쿼리 2 풀이

--- src/main/java/com/b1uffer/day6/Q05.java
+++ src/main/java/com/b1uffer/day6/Q05.java
@@ -6,18 +6,20 @@ public class Q05 {
     public int[] solution(int[] arr, int[][] queries) {
         int[] answer = {};
         int[] ar = new int[queries.length];
-        int min = -1;
-
         for(int i=0; i<queries.length; i++) {
+            int min = -1;
             for(int j = queries[i][0]; j <= queries[i][1]; j++) {
-                int k = queries[i][2];
-                if(arr[j] > k) {
-                    if(arr[j] < min) {
+                int k = queries[i][2]; // 이걸 기준으로 비교
+                if(arr[j] > k) { // k보다 크므로 무조건 양수
+                    if(min == -1) {
+                        min = arr[j]; // 첫번째 값은 무조건 이 if문을 거친다
+                    } else if(arr[j] < min) {
                         min = arr[j];
                     }
                 }
-            }
-        }
+            } // j for문 끝
+            ar[i] = min;
+        } // i for문 끝
         answer = ar;
         return answer;
     }
